a6e4155192ad69a3ce4a47512739088b1f95d34c
mousa1995 <mousasoccerlove@gmail.com>
2026-09-09 17:54:57 +0330

    solve: (Easy) oneList

---
+++ edabit/easy/Two-Lists-inside-a-List-to-One.js
@@ -0,0 +1,9 @@
+//https://edabit.com/challenge/ccv2_22a5a19b21ee9fd935d08a89
+const oneList = (lists) => lists.flat(Infinity);
+
+console.log(
+  oneList([
+    [1, 2],
+    [3, 4],
+  ]),
+); //➞ [1,2,3,4]
